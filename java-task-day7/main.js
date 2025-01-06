15e548a89edd2d8afb8f3b1a921dceb304d8195c
abdkrakash <ABDKARKASH@GMAIL.COM>
2025-01-06 16:54:08 +0300

    task1-12

---
+++ java-task-day7/main.js
@@ -0,0 +1,111 @@
+// task1------
+// var user = {
+//     name: "abd",
+//     age: "27",
+//     gender:"male",
+// }
+// console.log(`${user.name} , ${user.age},${user.gender}`)
+
+
+// task2------
+// var user = {
+
+//     name: "abd",
+//     age: "27",
+// }
+// user.gender = "male"
+// console.log(`${user.name} , ${user.age},${user.gender}`)
+
+// task3-----
+
+// var user = {
+
+//     name: "abd",
+//     age: "27",
+// }
+// user.gender = "male"
+// console.log(user["name"] , user["age"],user["gender"])
+
+// task4-----
+// var user = {
+//     name: "abd",
+//     age: "27",
+//     gender:"male",
+// }
+// console.log(user.name )
+
+// task5-----
+// var user = {
+//     name: "abd",
+//     age: "27",
+//     gender:"male",
+// }
+// console.log(user["name"] )
+
+
+// task6----
+
+// var user = {
+//     name: "abd",
+//     age: "27",
+//     gender:"male",
+// }
+// for (key in user){
+// console.log(user[key] )
+// }
+
+// task7----
+
+// var user = {
+//     name: "abd",
+//     age: "27",
+//     gender:"male",
+// }
+// console.log (Object.keys(user))
+
+// task8----
+// var user = {
+//     name: "abd",
+//     age: "27",
+//     gender:"male",
+// }
+// console.log (Object.values(user))
+// // task9----
+
+// var user = {
+//     name: "abd",
+//     age: "27",
+//     gender:"male",
+// }
+// console.log (Object.entries(user))
+// // task10----
+// var user = {
+//     name: "abd",
+//     age: "27",
+// }
+
+// var usre2 ={
+//      gender:"male"}
+
+// console.log (Object.assign(user, usre2))
+
+
+// task11----
+
+// var user = {
+//     name: "abd",
+//     age: "27",
+// }
+// console.log (Object.freeze(user))
+// var usre2 ={
+//          gender:"male"}
+
+// task12----
+// var user = {
+//     name: "abd",
+//     age: "27",
+//     gender:"male",
+// }
+// Object.seal(user);
+// user.name = "moh";
+// console.log(user.name);
